a39f6adb42ff12bd90e3af14449de52513565363
codebox workspace user <darkliquid@darkliquid.co.uk>
2014-01-25 18:00:16 +0000

    Binning daemon stuff for now, lets just focus on getting stuff doing it's job first

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main;
 
 import (
-    "github.com/VividCortex/godaemon"
+    "flag"
+    "os"
+    "path/filepath"
 )
 
 func main() {
-    godaemon.MakeDaemon(&godaemon.DaemonAttr{})
+    // Gets the current executable path for use a cfgfile path
+    cwd, err := os.Getwd()
+
+    // Bail out if the executable can not be found
+    if err != nil {
+        panic("Can not find current working directory!")
+    }
+
+    // Default config path
+    config_path := filepath.Join(cwd, "leader-1.json")
+
+    // Allow setting of the cfg file to load stuff from
+    flag.StringVar(&config_path, "cfg", config_path, "sets the config file")
+
+    flag.Parse()
 }
