456680071fa857676c1d812ab65fb807cfd5248e
codebox workspace user <darkliquid@darkliquid.co.uk>
2014-01-25 19:52:10 +0000

    Config bits, parsing, basic sig-handling

---
+++ config/config.go
@@ -0,0 +1,83 @@
+package config
+
+import (
+	"encoding/json"
+	"flag"
+	"github.com/fluffle/golog/logging"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+type IrcSettings struct {
+	Host     string
+	Port     string
+	Nick     string
+	Pass     string
+	Ssl      bool
+	Channels []string
+}
+
+type DbSettings struct {
+	User string
+	Pass string
+	Host string
+	Port string
+}
+
+type Settings struct {
+	Irc IrcSettings
+	Db  DbSettings
+}
+
+var Config Settings
+
+func Load() {
+	// Gets the current executable path for use a cfgfile path
+	cwd, err := os.Getwd()
+
+	// Bail out if the executable can not be found
+	if err != nil {
+		// Init here since we haven't done so yet
+		logging.InitFromFlags()
+		logging.Fatal("Can not find current working directory!")
+	}
+
+	// Default config path
+	config_path := filepath.Join(cwd, "leader-1.json")
+
+	// Allow setting of the cfg file to load stuff from
+	flag.StringVar(&config_path, "cfg", config_path, "sets the config file")
+
+	// Parse command line settings
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+
+	// Need to init logging engine
+	logging.InitFromFlags()
+
+	logging.Info("Loading...")
+
+	// Load config file
+	file, err := ioutil.ReadFile(config_path)
+
+	// Bail out if reading the config file errored
+	if err != nil {
+		logging.Fatal("Couldn't read config file")
+	}
+
+	logging.Info("Reading config file")
+
+	// Unmarshal json config file into our config structure
+	err = json.Unmarshal(file, &Config)
+
+	// Bail out if the demarshalling fails
+	if err != nil {
+		logging.Fatal("Couldn't parse config!")
+	}
+
+	logging.Info("Loaded config")
+
+	return
+}
--- main.go
+++ main.go
@@ -1,25 +1,60 @@
-package main;
+package main
 
 import (
-    "flag"
-    "os"
-    "path/filepath"
+	"fmt"
+	"github.com/darkliquid/leader1/config"
+	irc "github.com/fluffle/goirc/client"
+	"github.com/fluffle/golog/logging"
+	"os"
+	"os/signal"
 )
 
 func main() {
-    // Gets the current executable path for use a cfgfile path
-    cwd, err := os.Getwd()
+	// Load our configuration
+	config.Load()
 
-    // Bail out if the executable can not be found
-    if err != nil {
-        panic("Can not find current working directory!")
-    }
+	// Alias it for easy use
+	cfg := config.Config
 
-    // Default config path
-    config_path := filepath.Join(cwd, "leader-1.json")
+	// Set up Irc Client
+	c := irc.SimpleClient(cfg.Irc.Nick)
 
-    // Allow setting of the cfg file to load stuff from
-    flag.StringVar(&config_path, "cfg", config_path, "sets the config file")
+	// Optionally, enable SSL
+	c.SSL = cfg.Irc.Ssl
 
-    flag.Parse()
+	// Join channels on connect
+	c.AddHandler(irc.CONNECTED, func(conn *irc.Conn, line *irc.Line) {
+		for _, channel := range cfg.Irc.Channels {
+			conn.Join(channel)
+			logging.Info(fmt.Sprintf("Joining channel %s", channel))
+		}
+	})
+
+	// And a signal on disconnect
+	quit := make(chan bool)
+	c.AddHandler(irc.DISCONNECTED,
+		func(conn *irc.Conn, line *irc.Line) { quit <- true })
+
+	logging.Info(fmt.Sprintf("Connection to %s:%s", cfg.Irc.Host, cfg.Irc.Port))
+
+	// Tell client to connect
+	if err := c.Connect(cfg.Irc.Host + ":" + cfg.Irc.Port); err != nil {
+		// At the moment, just fail, but ideally we will retry until a maximum number of failures is exceeded
+		logging.Fatal(fmt.Sprintf("Connection error: %s\n", err.Error()))
+	}
+	logging.Info(fmt.Sprintf("Connected to irc server as %s", cfg.Irc.Nick))
+
+    // Trap interrupt signal so we can cleanly disconnect on fail
+	trap := make(chan os.Signal, 1)
+    signal.Notify(trap, os.Interrupt)
+
+    go func(){
+        for sig := range trap {
+            c.Quit(fmt.Sprintf("Goodbye (%s)", sig))
+            quit <- true
+        }
+    }()
+
+	// Wait for disconnect
+	<-quit
 }
