8af2a28184cf501e45517922b82e919ccaa5cc95
codebox workspace user <darkliquid@darkliquid.co.uk>
2014-02-08 15:46:48 +0000

    Bug fixes

--- commands/like_hate.go
+++ commands/like_hate.go
@@ -43,6 +43,7 @@ func doLikeOrHate(conn *irc.Conn, line *irc.Line, target string, likeType string
 
     if strings.TrimSpace(stats.SongTitle) == "" {
         logging.Error(fmt.Sprintf("Song title missing for some reason when do a %s for %s at %s", likeType, line.Nick, time.Now()))
+        return
     }
 
 	rows, err := db.Query("INSERT INTO likelogs (type, user, song, date) VALUES (?, ?, ?, ?)", likeType, line.Nick, stats.SongTitle, time.Now().Unix())
